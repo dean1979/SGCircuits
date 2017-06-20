b83159ce33d7a2f53b9a712444ab202a503c86bf
dean <99092651@qq.com>
2017-06-20 16:17:51 +0800

    加入滑动菜单

--- app/src/main/java/com/dsc365/sgcircuits/MainActivity.java
+++ app/src/main/java/com/dsc365/sgcircuits/MainActivity.java
@@ -1,5 +1,8 @@
 package com.dsc365.sgcircuits;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +12,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         //设置TOOLBar
         Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
         this.setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) this.findViewById(R.id.drawer_layout);
+        ActionBar actionBar = this.getSupportActionBar();//由toolbar 实现 了此处
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);//让导航按钮显示出来
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);//设置导航按钮图标
+        }
     }
 
     @Override
@@ -27,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home: //HomeASUp 按钮的id永远是此值
+                drawerLayout.openDrawer(GravityCompat.START);//调用滑动菜单显示
+                break;
             case R.id.settings:
                 Toast.makeText(this,"you clicked settings",Toast.LENGTH_SHORT).show();
                 break;
